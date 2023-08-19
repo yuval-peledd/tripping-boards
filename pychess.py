96bad39037d5328c0e9d216688708a29d078c754
Roy Rashti <roy@island.io>
2023-08-19 17:08:47 +0300

    Made pychess run async

--- pychess.py
+++ pychess.py
@@ -4,7 +4,7 @@ Run this file to launch the program.
 
 In this file, we handle the main menu which gets displayed at runtime.
 """
-import sys  
+import sys
 import pygame
 
 import chess
@@ -12,6 +12,15 @@ import menus
 from tools.loader import MAIN
 from tools import sound
 
+import asyncio
+from aiohttp import web
+# Define the custom event type
+CUSTOM_EVENT = pygame.USEREVENT + 1
+
+# Create an instance of the custom event
+custom_event = pygame.event.Event(CUSTOM_EVENT)
+
+import asyncio
 # This is a non-important bit of code. Flush stdout - useful incase external
 # programs are calling this application.
 sys.stdout.flush()
@@ -29,10 +38,10 @@ pygame.display.set_caption("My-PyChess")
 pygame.display.set_icon(MAIN.ICON)
 
 # Coordinates of buttons in rectangle notation.
-sngl = (260, 140, 220, 40)
-mult = (280, 200, 200, 40)
-onln = (360, 260, 120, 40)
-load = (280, 320, 200, 40)
+sngl = (260, 180, 220, 40)
+mult = (280, 240, 200, 40)
+onln = (360, 300, 120, 40)
+load = (280, 360, 200, 40)
 pref = (0, 450, 210, 40)
 abt = (390, 450, 110, 40)
 hwto = (410, 410, 90, 30)
@@ -102,116 +111,125 @@ prefs = menus.pref.load()
 
 music = sound.Music()
 music.play(prefs)
-while run:
-    # Start the game loop at 30fps, show the screen every time at first
-    clock.tick(30)
-    showMain(prefs)
 
-    # We need to get the position of the mouse so that we can blit an image
-    # on the text over which the mouse hovers
-    x, y = pygame.mouse.get_pos()
+async def main():
+    # Rest of your script can continue running here
+    while True:
+        # Start the game loop at 30fps, show the screen every time at first
+        clock.tick(30)
+        global prefs
+        showMain(prefs)
 
-    if sngl[0] < x < sum(sngl[::2]) and sngl[1] < y < sum(sngl[1::2]):
-        win.blit(MAIN.SINGLE_H, sngl[:2])
+        # We need to get the position of the mouse so that we can blit an image
+        # on the text over which the mouse hovers
+        x, y = pygame.mouse.get_pos()
 
-    if mult[0] < x < sum(mult[::2]) and mult[1] < y < sum(mult[1::2]):
-        win.blit(MAIN.MULTI_H, mult[:2])
+        if sngl[0] < x < sum(sngl[::2]) and sngl[1] < y < sum(sngl[1::2]):
+            win.blit(MAIN.SINGLE_H, sngl[:2])
 
-    if onln[0] < x < sum(onln[::2]) and onln[1] < y < sum(onln[1::2]):
-        win.blit(MAIN.ONLINE_H, onln[:2])
+        if mult[0] < x < sum(mult[::2]) and mult[1] < y < sum(mult[1::2]):
+            win.blit(MAIN.MULTI_H, mult[:2])
 
-    if load[0] < x < sum(load[::2]) and load[1] < y < sum(load[1::2]):
-        win.blit(MAIN.LOAD_H, load[:2])
+        if onln[0] < x < sum(onln[::2]) and onln[1] < y < sum(onln[1::2]):
+            win.blit(MAIN.ONLINE_H, onln[:2])
 
-    if pref[0] < x < sum(pref[::2]) and pref[1] < y < sum(pref[1::2]):
-        win.blit(MAIN.PREF_H, pref[:2])
-        
-    if hwto[0] < x < sum(hwto[::2]) and hwto[1] < y < sum(hwto[1::2]):
-        win.blit(MAIN.HOWTO_H, hwto[:2])
+        if load[0] < x < sum(load[::2]) and load[1] < y < sum(load[1::2]):
+            win.blit(MAIN.LOAD_H, load[:2])
 
-    if abt[0] < x < sum(abt[::2]) and abt[1] < y < sum(abt[1::2]):
-        win.blit(MAIN.ABOUT_H, abt[:2])
+        if pref[0] < x < sum(pref[::2]) and pref[1] < y < sum(pref[1::2]):
+            win.blit(MAIN.PREF_H, pref[:2])
 
-    if stok[0] < x < sum(stok[::2]) and stok[1] < y < sum(stok[1::2]):
-        win.blit(MAIN.STOCK_H, stok[:2])
+        if hwto[0] < x < sum(hwto[::2]) and hwto[1] < y < sum(hwto[1::2]):
+            win.blit(MAIN.HOWTO_H, hwto[:2])
 
-    # Begin pygame event loop to catch all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+        if abt[0] < x < sum(abt[::2]) and abt[1] < y < sum(abt[1::2]):
+            win.blit(MAIN.ABOUT_H, abt[:2])
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # User has clicked somewhere, determine which button and
-            # call a function to handle the game into a different window.
-            x, y = event.pos
+        if stok[0] < x < sum(stok[::2]) and stok[1] < y < sum(stok[1::2]):
+            win.blit(MAIN.STOCK_H, stok[:2])
 
-            if sngl[0] < x < sum(sngl[::2]) and sngl[1] < y < sum(sngl[1::2]):
-                sound.play_click(prefs)
-                ret = menus.splayermenu(win)
-                if ret == 0:
-                    run = False
-                elif ret != 1:
-                    if ret[0]:
-                        run = chess.mysingleplayer(win, ret[1], prefs)
+        # Begin pygame event loop to catch all events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                print("quit")
+            elif event.type == CUSTOM_EVENT:
+                print("Custom event received!")
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # User has clicked somewhere, determine which button and
+                # call a function to handle the game into a different window.
+                x, y = event.pos
+
+                if sngl[0] < x < sum(sngl[::2]) and sngl[1] < y < sum(sngl[1::2]):
+                    sound.play_click(prefs)
+                    ret = menus.splayermenu(win)
+                    if ret == 0:
+                        run = False
+                    elif ret != 1:
+                        if ret[0]:
+                            run = chess.mysingleplayer(win, ret[1], prefs)
+                        else:
+                            run = chess.singleplayer(win, ret[1], ret[2], prefs)
+
+                elif mult[0] < x < sum(mult[::2]) and mult[1] < y < sum(mult[1::2]):
+                    sound.play_click(prefs)
+                    ret = menus.timermenu(win, prefs)
+                    if ret == 0:
+                        run = False
+                    elif ret != 1:
+                        run = await chess.multiplayer(win, ret[0], ret[1], prefs)
+
+                elif onln[0] < x < sum(onln[::2]) and onln[1] < y < sum(onln[1::2]):
+                    sound.play_click(prefs)
+                    ret = menus.onlinemenu(win)
+                    if ret == 0:
+                        run = False
+                    elif ret != 1:
+                        run = chess.online(win, ret[0], prefs, ret[1])
+
+                elif load[0] < x < sum(load[::2]) and load[1] < y < sum(load[1::2]):
+                    sound.play_click(prefs)
+                    ret = menus.loadgamemenu(win)
+                    if ret == 0:
+                        run = False
+
+                    elif ret != 1:
+                        if ret[0] == "multi":
+                            run = chess.multiplayer(win, *ret[1:3], prefs, ret[3])
+                        elif ret[0] == "single":
+                            run = chess.singleplayer(win, *ret[1:3], prefs, ret[3])
+                        elif ret[0] == "mysingle":
+                            run = chess.mysingleplayer(win, ret[1], prefs, ret[2])
+
+                elif pref[0] < x < sum(pref[::2]) and pref[1] < y < sum(pref[1::2]):
+                    sound.play_click(prefs)
+                    run = menus.prefmenu(win)
+
+                    prefs = menus.pref.load()
+                    if music.is_playing():
+                        if not prefs["sounds"]:
+                            music.stop()
                     else:
-                        run = chess.singleplayer(win, ret[1], ret[2], prefs)
+                        music.play(prefs)
 
-            elif mult[0] < x < sum(mult[::2]) and mult[1] < y < sum(mult[1::2]):
-                sound.play_click(prefs)
-                ret = menus.timermenu(win, prefs)
-                if ret == 0:
-                    run = False
-                elif ret != 1:
-                    run = chess.multiplayer(win, ret[0], ret[1], prefs)
+                elif hwto[0] < x < sum(hwto[::2]) and hwto[1] < y < sum(hwto[1::2]):
+                    sound.play_click(prefs)
+                    run = menus.howtomenu(win)
 
-            elif onln[0] < x < sum(onln[::2]) and onln[1] < y < sum(onln[1::2]):
-                sound.play_click(prefs)
-                ret = menus.onlinemenu(win)
-                if ret == 0:
-                    run = False
-                elif ret != 1:
-                    run = chess.online(win, ret[0], prefs, ret[1])
+                elif abt[0] < x < sum(abt[::2]) and abt[1] < y < sum(abt[1::2]):
+                    sound.play_click(prefs)
+                    run = menus.aboutmenu(win)
 
-            elif load[0] < x < sum(load[::2]) and load[1] < y < sum(load[1::2]):
-                sound.play_click(prefs)
-                ret = menus.loadgamemenu(win)
-                if ret == 0:
-                    run = False
+                elif stok[0] < x < sum(stok[::2]) and stok[1] < y < sum(stok[1::2]):
+                    sound.play_click(prefs)
+                    run = menus.sfmenu(win)
 
-                elif ret != 1:
-                    if ret[0] == "multi":
-                        run = chess.multiplayer(win, *ret[1:3], prefs, ret[3])
-                    elif ret[0] == "single":
-                        run = chess.singleplayer(win, *ret[1:3], prefs, ret[3])
-                    elif ret[0] == "mysingle":
-                        run = chess.mysingleplayer(win, ret[1], prefs, ret[2])
+        # Update the screen every frame
+        pygame.display.flip()
+        await asyncio.sleep(1)
 
-            elif pref[0] < x < sum(pref[::2]) and pref[1] < y < sum(pref[1::2]):
-                sound.play_click(prefs)
-                run = menus.prefmenu(win)
-                
-                prefs = menus.pref.load()
-                if music.is_playing():
-                    if not prefs["sounds"]:
-                        music.stop()
-                else:
-                    music.play(prefs)
-                    
-            elif hwto[0] < x < sum(hwto[::2]) and hwto[1] < y < sum(hwto[1::2]):
-                sound.play_click(prefs)
-                run = menus.howtomenu(win)
-
-            elif abt[0] < x < sum(abt[::2]) and abt[1] < y < sum(abt[1::2]):
-                sound.play_click(prefs)
-                run = menus.aboutmenu(win)
-
-            elif stok[0] < x < sum(stok[::2]) and stok[1] < y < sum(stok[1::2]):
-                sound.play_click(prefs)
-                run = menus.sfmenu(win)
-
-    # Update the screen every frame
-    pygame.display.flip()
-
+if __name__ == '__main__':
+    asyncio.run(main())
 # Stop music, quit pygame after the loop is done
 music.stop()
 pygame.quit()
